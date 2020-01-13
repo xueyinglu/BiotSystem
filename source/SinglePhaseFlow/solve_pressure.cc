1c7bc3c7fe1847855292907f78ba9f12ecb27421
xueyinglu <xylu@utexas.edu>
2020-01-13 16:24:50 -0600

    clear duplicated files

--- source/SinglePhaseFlow/solve_pressure.cc
+++
@@ -1,9 +0,0 @@
-#include "BiotSystem.h"
-void BiotSystem::solve_pressure(){
-    SolverControl solver_control(1000, 1e-12);
-    SolverCG<> solver(solver_control);
-    PreconditionSSOR<> preconditioner;
-    preconditioner.initialize(system_matrix_pressure, 1.2);
-    solver.solve(system_matrix_pressure, solution_pressure, system_rhs_pressure, preconditioner);
-    //constraints_pressure.distribute(solution_pressure);
-}
