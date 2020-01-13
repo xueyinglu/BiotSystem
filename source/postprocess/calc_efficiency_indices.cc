5fa1f29fa0da3e5aef47446a9c1fcb082fe2b0f6
xueyinglu <xylu@utexas.edu>
2020-01-13 16:29:45 -0600

    modified efficiency table content

--- source/postprocess/calc_efficiency_indices.cc
+++ source/postprocess/calc_efficiency_indices.cc
@@ -5,9 +5,10 @@ void BiotSystem::calc_efficiency_indices(){
     +eta_face_partial_sigma.back() + eta_face_sigma.back() + eta_partial_u.back()+eta_u.back());
 
     double error = l2_error_p.back()*l2_error_p.back()*biot_inv_M/4 + energy_error_u.back()*energy_error_u.back();
-    double I_eff = eta_sum.back()/error;
+    double I_eff = sqrt(eta_sum.back()/error);
 
     efficiency_table.add_value("time", t);
     efficiency_table.add_value("error", error);
+    efficiency_table.add_value("eta_sum", eta_sum.back());
     efficiency_table.add_value("index", I_eff);
 }
